b949c43ff4ec64d4b50c75b1eac8c873a35ee66d
tdunne <saxbassman@hotmail.com>
2014-01-18 16:32:32 +1100

    Edited default config

--- caesar.py
+++ caesar.py
@@ -36,27 +36,6 @@ def create_default_config(fn):
    admins = [owner, 'someoneyoutrust']
    # But admin.py, mcstatus.py and services.py are disabled by default, as follows:
    exclude = ['admin', 'mcstatus', 'services']
-
-   # Services
-   # Services are disabled by default, see the exclude list above
-   # Email address for registering the bot with, will be used for all nick registrations
-   email = 'emailhere'
-   # NickServ
-   # NickServ password for the bot to login with
-   nickservpass = 'password'
-   # HostServ
-   # This will request a vhost for your bot once it's nick is registered
-   vhost = 'vhost.you.want'
-   # ChanServ
-   # Logging in to channels
-   # cshannel is the channel you want to log in to, chanservpass is it's password
-   cschannel = '#channel'
-   chanservpass = 'password'
-   
-   # Autovoicing
-   # caesar will autovoice anyone who joins any of the channels he is op in when enabled
-   # However, autovoicing is disabled by default:
-   autovoice = False
    
    # If you want to enumerate a list of modules rather than disabling
    # some, use "enable = ['example']", which takes precedent over exclude
