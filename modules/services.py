7a15e01ea710dd51a2616113d9884f8e743fd935
tdunne <saxbassman@hotmail.com>
2014-01-21 00:19:30 +1100

    Update services.py

--- modules/services.py
+++ modules/services.py
@@ -14,8 +14,6 @@ def nickservreg(caesar, input):
 	email = caesar.config.email
 	password = caesar.config.nickservpass
 	if input.sender.startswith('#'): return
-	if email == 'emailhere' and password == 'password':
-		caesar.reply("Edit the default config first")
 	if input.owner:
 		caesar.write(['PRIVMSG', 'NICKSERV', 'REGISTER', password, email])
 nickservreg.commands = ['nsregister'] # Should only have to be used once. Syntax: !nsregister
@@ -23,11 +21,8 @@ nickservreg.priority = 'medium'
 
 def nslogin(caesar, input):
 	'''log in to NickServ'''
-	# Must be done through privmsg with admin
+	# Admin only command
 	password = caesar.config.nickservpass
-	if password == 'password':
-		phenny.reply("Edit the default config first")
-	if input.sender.startswith('#'): return # must be done through privmsg
 	if input.admin:
 		caesar.write(['PRIVMSG', 'NICKSERV', 'IDENTIFY', password])
 nslogin.commands = ['nslogin'] # Syntax !nslogin
@@ -44,8 +39,6 @@ nsupdate.priority = 'medium'
 def hostservrequest(caesar, input):
 	'''request a vhost'''
 	vhost = caesar.config.vhost
-	if vhost == 'vhost.you.want':
-		caesar.reply("Edit the default config first")
 	if input.sender.startswith('#'): return # Must be done through privmsg and...
 	if input.owner: # ...by the owner
 		caesar.write(['PRIVMSG', 'HOSTSERV', 'REQUEST', vhost])
@@ -53,24 +46,13 @@ hostservrequest.commands = ['vhost'] # Syntax !vhost
 hostservrequest.priority = 'medium'
 
 def cslogin(caesar, input):
-		'''log into a channel through ChanServ'''
-		channel = input.group(2)
-		password = input.group(3)
-		if not channel and not password: # if other inputs are not added, use the channel and pass defined in the config
-			channel = caesar.config.cschannel
-			password = caesar.config.chanservpass
-			if channel == '#channel' and password == 'password':
-				caesar.reply("Edit the default config first")
-			if input.sender.startswith('#'): return # Must be done through privmsg and...
-			if input.owner: # ...by the owner
-				caesar.write(['PRIVMSG', 'CHANSERV', 'IDENTIFY', channel, password])
-		else: # if other inputs are added, use them instead of the config values
-			channel = input.group(2)
-			password = input.group(3)
-			if input.sender.startswith('#'): return # Must be done through privmsg and...
-			if input.owner: # ...by the owner
-				caesar.write(['PRIVMSG', 'CHANSERV', 'IDENTIFY', channel, password])
-cslogin.commands = ['cslogin'] # Syntax !cslogin or (optional) !cslogin #channel password
+	'''log into a channel through ChanServ'''
+		channel = caesar.config.cschannel
+		password = caesar.config.chanservpass
+		if input.sender.startswith('#'): return # Must be done through privmsg and...
+		if input.owner: # ...by the owner
+			caesar.write(['PRIVMSG', 'CHANSERV', 'IDENTIFY', channel, password])
+cslogin.commands = ['cslogin'] # Syntax !cslogin
 cslogin.priority = 'medium'
 
 if __name__ == '__main__': 
